dbe741d3daffe5115f43f622cb654117798a6be4
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-03-11 16:38:11 +0100

    class Timer and constructor function

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 console.log("Hi There!");
+// Things our program does
+
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    // Assign 3 arguments to instance variables
+    // So we can refer back to them later from other methods in the class
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    // Add event listeners to the buttons
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+    this.durationInput.addEventListener("change", this.onDurationChange);
+  }
+
+  start() {
+    console.log("Time to start the timer!");
+  }
+
+  pause() {
+    console.log("Time to pause the timer!");
+  }
+
+  onDurationChange() {
+    console.log(this.durationInput.value);
+  }
+
+  tick() {}
+}
+
+new Timer(
+  document.querySelector("#duration"),
+  document.querySelector("#start"),
+  document.querySelector("#pause")
+);
